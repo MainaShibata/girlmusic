ffbdc60d272c563f28132a48494a7c9db5cd5a40
Shibata Maina <lemondm7@gmail.com>
2014-07-11 23:48:01 +0900

    new set file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$id = mysql_connect("http://localhost:3306/db","root","");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+  <p><?php echo 'Hello,world?' ?></p>
+</body>
+</html>
