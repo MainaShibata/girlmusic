d6a851cc2fe964a669e4e3b4b2e55f9206c0c5a3
Shibata Maina <lemondm7@gmail.com>
2014-07-12 01:25:10 +0900

    create search and delete test file

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ $id = mysql_connect("http://localhost:3306/db","root","");
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
-<body>
-  <p><?php echo 'Hello,world?' ?></p>
+<body class="index">
+  <h1><img src="img/logo.png"></h1>
+  <footer><p>copyright&copy;2014 Maina Shibata</p></footer>
 </body>
 </html>
---
+++ search.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body class="search pages">
+  <header>
+  <h1><img src="img/logo.png"></h1>
+  </header>
+  <form method="post">
+    <input type="text" placeholder="アーティスト名、曲名">
+    <input type="submit">
+  </form>
+</body>
+</html>
