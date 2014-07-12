f638572d59e2c5d49c7a0b803f3863555f92d80d
Shibata Maina <lemondm7@gmail.com>
2014-07-12 09:48:49 +0900

    add new img and create result page

---
+++ result.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body class="result pages">
+  <header>
+  <h1><img src="img/logo.png"></h1>
+  </header>
+</body>
+</html>
