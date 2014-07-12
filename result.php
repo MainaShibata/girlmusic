af5efdff05380abbaefddf5c4810686255a90a45
Shibata Maina <lemondm7@gmail.com>
2014-07-12 10:35:28 +0900

    update result page view

--- result.php
+++ result.php
@@ -13,5 +13,20 @@
   <header>
   <h1><img src="img/logo.png"></h1>
   </header>
+  <form action="result_submit" method="get" accept-charset="utf-8">
+    <input type="text">
+  </form>
+  <ul>
+    <li class="on">
+      <img src="http://lorempixel.com/200/200/fashion">
+      <div class="share"><img src="img/icon_share.png"></div>
+    </li>
+    <li>
+      <img src="http://lorempixel.com/200/200/fashion">
+    </li>
+    <li>
+      <img src="http://lorempixel.com/200/200/fashion">
+    </li>
+  </ul>
 </body>
 </html>
